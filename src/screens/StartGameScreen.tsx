7d6d8ed13f6d1bc915bc7f161264e3a726e48119
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-03-28 14:42:27 -0300

    style(App.tsx): comment out unused styles in App component feat(PrimaryButton.tsx): add Pressable component and onPress handler to PrimaryButton component, add styles for button and pressed state
    feat(StartGameScreen.tsx): add styling to input container and number input field

--- src/screens/StartGameScreen.tsx
+++ src/screens/StartGameScreen.tsx
@@ -1,16 +1,53 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, StyleSheet } from "react-native";
 import React from "react";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput placeholder="Number" />
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Number"
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
 
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+
+    // Shadows
+    elevation: 4,
+    // Shadow for iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 
 export default StartGameScreen;
